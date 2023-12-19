ca90d8da7328438cb604416069a4a97818bbbf44
Charles Coulombe <charles.coulombe@calculquebec.ca>
2023-12-19 11:47:10 -0500

    Updated configs for MC 13.1.0.
    Use node partition from form partition field

--- beluga/cirq/main.tf
+++ beluga/cirq/main.tf
@@ -1,11 +1,11 @@
 terraform {
-  required_version = ">= 1.2.1"
+  required_version = ">= 1.4.0"
 }
 
 module "openstack" {
-  source         = "git::https://github.com/ComputeCanada/magic_castle.git//openstack?ref=12.6.2"
+  source         = "git::https://github.com/ComputeCanada/magic_castle.git//openstack?ref=13.1.0"
   config_git_url = "https://github.com/ComputeCanada/puppet-magic_castle.git"
-  config_version = "12.6.2"
+  config_version = "13.1.0"
 
   cluster_name = "cirq"
   domain       = "calculquebec.cloud"
@@ -44,8 +44,9 @@ output "public_ip" {
 
 # Uncomment to register your domain name with CloudFlare
 module "dns" {
-  source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref=12.6.2"
-  email            = chomp(file("../../.myemail.txt"))
+  source           = "git::https://github.com/ComputeCanada/magic_castle.git//dns/cloudflare?ref=13.1.0"
+#  email            = chomp(file("../../.myemail.txt"))
+  bastions         = module.openstack.bastions
   name             = module.openstack.cluster_name
   domain           = module.openstack.domain
   public_instances = module.openstack.public_instances
