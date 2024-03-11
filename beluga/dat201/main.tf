a4f2116303ed8970e41dd9dbf56223575d278bdb
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-03-11 17:33:51 -0400

    recreate nodes

--- beluga/dat201/main.tf
+++ beluga/dat201/main.tf
@@ -28,7 +28,7 @@ module "openstack" {
   instances = {
     mgmt   = { type = "p4-7.5gb", tags = ["puppet", "mgmt", "nfs"], count = 1 }
     login  = { type = "p4-7.5gb", tags = ["login", "public", "proxy"], count = 1 }
-    node   = { type = "c8-60gb", tags = ["node"], count = 0 }
+    node   = { type = "c8-60gb", tags = ["node"], count = 2 }
   }
 
   # var.pool is managed by Slurm through Terraform REST API.
