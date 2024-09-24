b0de156e180da4009965660361993a6b580be05e
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2024-09-24 13:47:58 -0400

    remove gpu node from ciq101, add numpy, pandas, matplotlib

--- ciq101/main.tf
+++ ciq101/main.tf
@@ -4,8 +4,6 @@ locals {
   custom = {
     nnode_cpu = 0
     nnode_cpupool = 4
-    nnode_gpupool = 1
-    image_cpu = "snapshot-cpunode-2024-R810.5"
-    image_gpu = "snapshot-gpunode-2024-R810.5"
+    nnode_gpupool = 0
   }
 }
