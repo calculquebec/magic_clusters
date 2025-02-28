01ff72acc907b95c1d16b1d475fc0d0e110097a0
Elie Haber <12099465+elielhaber@users.noreply.github.com>
2025-02-28 14:29:01 -0500

    Remove GPU configurations

--- cip201/main.tf
+++ cip201/main.tf
@@ -5,12 +5,6 @@ locals {
     nnodes = {
       cpu = 1
       cpupool = 8
-      gpu = 2
-      gpupool = 10
-    }
-    mig = {
-      gpu = { "2g.10gb" = 3 }
-      gpupool = { "2g.10gb" = 3 }
     }
   }
 }
