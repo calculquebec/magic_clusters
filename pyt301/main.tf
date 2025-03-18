ee4515684fb5aad2c95ec719c6ff56e610a0de85
Pier-Luc St-Onge <plstonge@users.noreply.github.com>
2025-03-18 12:10:27 -0400

    PYT301 - 6 inscriptions donc 10 instances MIG

--- pyt301/main.tf
+++ pyt301/main.tf
@@ -3,9 +3,9 @@ locals {
   
   custom = {
     nnodes = {
-      cpu = 0
-      gpu = 0
-      gpupool = 4
+      cpu = 0      # Forcer 0 noeud CPU
+      gpu = 0      # Fonctionner uniquement avec les gpupool
+      gpupool = 5  # 6 inscriptions + 1 instructor + 3 helpers = 10 GI
     }
     mig = {
       gpupool = { "3g.20gb" = 2 }
