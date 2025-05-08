7c2791950a85b936b65fe23e3100c0eba9b8c810
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-05-08 10:15:25 -0400

    configure gpu nodes MIG

--- ecole/main.tf
+++ ecole/main.tf
@@ -6,10 +6,14 @@ locals {
       cpu = 0
       gpu = 0
       compute_node = 0
-      cpupool = 0
-      gpupool = 0
+      cpupool = 15
+      gpupool = 15
       # instance jupyter séparée
       jupyter = 1
+    }
+
+    mig = {
+      gpupool = { "3g.20gb" = 2 }
     }
 
     # taille des systèmes de fichiers. Les valeurs par défaut sont celles ci-dessous
