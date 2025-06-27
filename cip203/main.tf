efa4a52d5f4b6948f0ae212ba5017193702ef76d
Nikolai Sergueev <nikolai.sergueev@calculquebec.ca>
2025-06-27 16:26:48 -0400

    Updated main.tf

--- cip203/main.tf
+++ cip203/main.tf
@@ -5,10 +5,12 @@ locals {
     nnodes = {
       cpu = 0      # Forcer 0 noeud CPU
       gpu = 0      # Fonctionner uniquement avec les gpupool
-      gpupool = 5  # 6 inscriptions + 1 instructor + 3 helpers = 10 GI
+      gpupool = 5  # 5 users in total
+      gpupool2 = 10 # 5 users in total x 2 MIGs
     }
     mig = {
       gpupool = { "3g.20gb" = 2 }
+      gpupool2 = { "1g.5gb" = 7 }
     }
   }
 }
