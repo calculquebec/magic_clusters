f1dec1ffeb598a952e5fa5522ef108e1b8a1de52
Nikolai Sergueev <nikolai.sergueev@calculquebec.ca>
2025-06-27 18:23:28 -0400

    Changed number of nodes in main.tf

--- cip203/main.tf
+++ cip203/main.tf
@@ -5,7 +5,7 @@ locals {
     nnodes = {
       cpu = 0      # Forcer 0 noeud CPU
       gpu = 0      # Fonctionner uniquement avec les gpupool
-      gpupool = 2  # 2 users in total
+      gpupool = 1  # 1 mode is OK for testing
     }
     mig = {
       gpupool = { "3g.20gb" = 1, "1g.5gb" = 4}
