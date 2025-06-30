8c30a8517cd0a81bbc84a4023ea41a0ac2bfbb20
Nikolai Sergueev <nikolai.sergueev@calculquebec.ca>
2025-06-30 10:54:28 -0400

    Updated main.tf

--- cip203/main.tf
+++ cip203/main.tf
@@ -5,10 +5,10 @@ locals {
     nnodes = {
       cpu = 0      # Forcer 0 noeud CPU
       gpu = 0      # Fonctionner uniquement avec les gpupool
-      gpupool = 1  # 1 mode is OK for testing
+      gpupool = 2  # 1 mode is OK for testing
     }
     mig = {
-      gpupool = { "3g.20gb" = 1, "1g.5gb" = 4}
+      gpupool = {"1g.5gb" = 4}
     }
   }
 }
