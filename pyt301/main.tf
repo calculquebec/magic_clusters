3f4ee1a3952330499130dad6f5e8943ab9d08475
Nikolai Sergueev <nikolai.sergueev@calculquebec.ca>
2025-06-30 11:00:20 -0400

    Updated  main.tf for testing

--- pyt301/main.tf
+++ pyt301/main.tf
@@ -5,7 +5,7 @@ locals {
     nnodes = {
       cpu = 0      # Forcer 0 noeud CPU
       gpu = 0      # Fonctionner uniquement avec les gpupool
-      gpupool = 5  # 6 inscriptions + 1 instructor + 3 helpers = 10 GI
+      gpupool = 1  # 6 inscriptions + 1 instructor + 3 helpers = 10 GI
     }
     mig = {
       gpupool = { "3g.20gb" = 2 }
