b6d8e211791389ce687ea89f938dc27d5404457b
Denise Koch <94621966+kochdeni@users.noreply.github.com>
2025-10-08 11:52:26 -0400

    Update main.tf

--- cip101/main.tf
+++ cip101/main.tf
@@ -1,21 +1,10 @@
 locals {
   name = "cip101"
-  
+
   custom = {
     nnodes = {
       cpu = 1
-      compute_node = 1
-    }
-
-    instances_type_map = {
-      arbutus = {
-        cpu = "c8-60gb-186"
-	compute_node = "p8-12gb"
-      }
-      beluga = {
-        cpu = "c8-60gb"
-	compute_node = "p8-15gb"
-      }
+      cpupool = 8
     }
   }
 }
