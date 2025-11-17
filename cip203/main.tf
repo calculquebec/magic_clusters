3bcdb35cdee9ca2b437f313b70f8a16b0a139a43
Pier-Luc St-Onge <plstonge@users.noreply.github.com>
2025-11-17 11:35:58 -0500

    CIP203 - Un noeud GPU de plus

--- cip203/main.tf
+++ cip203/main.tf
@@ -5,7 +5,7 @@ locals {
     nnodes = {
       cpu = 0      # Forcer 0 noeud CPU
       gpu = 0      # Fonctionner uniquement avec les gpupool
-      gpupool = 4  # 1 mode is OK for testing
+      gpupool = 5  # 20 sessions Jupyter Lab (un 1g.5gb chaque)
     }
     mig = {
       gpupool = {"1g.5gb" = 4, "3g.20gb" = 1}
