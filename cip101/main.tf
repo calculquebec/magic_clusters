9e4e66ea854d86a784e66ca88d428ca8f6193785
Pier-Luc St-Onge <plstonge@users.noreply.github.com>
2025-11-17 11:12:42 -0500

    Indentation, commentaire sur compute_node

--- cip101/main.tf
+++ cip101/main.tf
@@ -4,17 +4,17 @@ locals {
   custom = {
     nnodes = {
       cpu = 1
-      compute_node = 1
+      compute_node = 1  # Requis pour les salloc et sbatch
     }
 
     instances_type_map = {
       arbutus = {
         cpu = "c8-60gb-186"
-	compute_node = "p8-12gb"
+        compute_node = "p8-12gb"
       }
       beluga = {
         cpu = "c8-60gb"
-	compute_node = "p8-15gb"
+        compute_node = "p8-15gb"
       }
     }
   }
