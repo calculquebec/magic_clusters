96edefe328eec511b52147923cc5acf776af5819
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2025-12-01 11:54:59 -0500

    removed unused network_map

--- common/common.tf
+++ common/common.tf
@@ -144,20 +144,6 @@ locals {
       gpupool12-j = null
     }
 
-    network_map = {
-      arbutus = {
-        subnet_id = null
-	os_ext_network = null
-      }
-      beluga = {
-        subnet_id = "a7f9fef1-a43e-4502-83a9-e47c936b635d"
-	os_ext_network = "Public-Network"
-      }
-      juno = {
-        subnet_id = "40981fb8-8421-455f-b691-75e5f52545f5"
-	os_ext_network = "Public-Network"
-      }
-    }
   }
 
   user_quotas = {
