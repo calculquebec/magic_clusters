7d7a933e377d74978fd04e360ba858049520ae15
Helene Gingras <helene.gingras@calculquebec.ca>
2026-01-14 11:23:12 -0500

    Ajustement pour le cours de Julie à UL 16-01-2026

--- dat101/main.tf
+++ dat101/main.tf
@@ -4,7 +4,7 @@ locals {
   custom = {
     nnodes = {
       cpu = 1      # Pour tester a l'avance
-      cpupool = 10  # 1 par personne : seulement 6 inscriptions + helpers
+      cpupool = 31  # 1 par personne : seulement 6 inscriptions + helpers
     }
 
     instances_type_map = {
