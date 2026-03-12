e25ab74c9c17ed12b748d85de99f3ab460e42da6
Nikolai Sergueev <nikolai.sergueev@calculquebec.ca>
2026-03-12 13:34:02 -0400

    Configured 7 MIGs per node

--- ia103/main.tf
+++ ia103/main.tf
@@ -1,10 +1,14 @@
 locals {
-  name = "ia102"
+  name = "ia103"
   
   custom = {
     nnodes = {
-      cpu = 0
-      cpupool = 2
+      cpu = 0      # Forcer 0 noeud CPU
+      gpu = 0      # Fonctionner uniquement avec les gpupool
+      gpupool = 1  # 20 sessions Jupyter Lab (un 1g.5gb chaque)
+    }
+    mig = {
+      gpupool = {"1g.5gb" = 7}
     }
   }
 }
