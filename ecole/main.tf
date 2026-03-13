30d02c86637673bab0b36b290ea52158bddb51eb
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2026-03-13 11:44:39 -0400

    preparation du cluster pour l'ecole

--- ecole/main.tf
+++ ecole/main.tf
@@ -3,21 +3,27 @@ locals {
  
   custom = {
     nnodes = {
-      cpu = 0
-      gpu = 0
+      cpu = 1
+      gpu = 1
       compute_node = 0
       cpupool = 15
       gpupool = 15
       # instance jupyter séparée
       jupyter = 1
+
+      # update git config
+      config_git_url = "https://github.com/computecanada/puppet-magic_castle.git"
+      config_version = "15.2.1"
     }
 
     mig = {
       gpupool = { "1g.5gb" = 7 }
     }
 
-    image_cpu = "snapshot-cpunode-2025-A9.4-ecole"
-    image_gpu = "snapshot-gpunode-2025-A9.4-ecole"
+    image_cpu = "AlmaLinux-9"
+    image_gpu = "AlmaLinux-9"
+#    image_cpu = "snapshot-cpunode-2025-A9.4-ecole"
+#    image_gpu = "snapshot-gpunode-2025-A9.4-ecole"
 
     # taille des systèmes de fichiers. Les valeurs par défaut sont celles ci-dessous
     home_size = 250
