c683ec9da9556af1cfe4c346070cef908b03491b
Nikolai Sergueev <nikolai.sergueev@calculquebec.ca>
2026-05-18 18:46:27 -0400

    Changed to 3g.20gb, gpupool=8

--- cip203/main.tf
+++ cip203/main.tf
@@ -5,10 +5,10 @@ locals {
     nnodes = {
       cpu = 0      # Forcer 0 noeud CPU
       gpu = 0      # Fonctionner uniquement avec les gpupool
-      gpupool = 5  # 20 sessions Jupyter Lab (un 1g.5gb chaque)
+      gpupool = 8  # 20 sessions Jupyter Lab (un 1g.5gb chaque)
     }
     mig = {
-      gpupool = {"1g.5gb" = 4, "3g.20gb" = 1}
+      gpupool = {"3g.20gb" = 2}
     }
   }
 }
